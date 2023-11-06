d3277c67731483a1e5d6dbb0dc7c8b2c2c5cde6d
reZerOR <gkraihan14k@gmail.com>
2023-11-06 15:37:26 +0600

    router added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="font-bold text-white text-5xl">this is home</h2>
+    </div>
+  );
+};
+
+export default Home;
